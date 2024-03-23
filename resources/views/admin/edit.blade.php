c428dd013bd9ab5cb12432f8cb8aaa25fa04c854
Abisarafi <Aubinskh123@student.uns.ac.id>
2024-03-23 16:16:35 +0700

    revisi edit nim

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -42,7 +42,7 @@
             @method('PUT')
             <div class="mb-3">
                 <label for="nim" class="form-label">NIM:</label>
-                <input type="text" class="form-control" id="nim" name="nim" value="{{ $mahasiswa->nim }}">
+                <input type="text" class="form-control" id="nim" name="nim" value="{{ $mahasiswa->nim }}" disabled>
             </div>
             <div class="mb-3">
                 <label for="nama" class="form-label">Nama:</label>
